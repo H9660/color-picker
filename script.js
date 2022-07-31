814ca4918fadaf7f17d0dd2def426735a2bd9c76
Hussain Lohawala <73359258+H9660@users.noreply.github.com>
2022-07-31 11:10:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,100 @@
+console.log("Welcome to quize da couleurs!");
+let win=new Audio("WON.wav");
+let lose=new Audio("LOSE.wav");
+let newgame=new Audio("NEW.wav");
+var generatedcolor;   // to be given to one of the choices
+var correct_choice;
+var won;
+
+//  function to generate the random numbers
+function generateRandom(maxLimit = 100){
+  let rand = Math.random() * maxLimit;
+  console.log(rand); // say 99.81321410836433
+
+  rand = Math.floor(rand); // 99
+
+  return rand;
+}
+
+
+function generatemaincolor(){
+
+     
+     correct_choice=generateRandom(4);
+     let rval=generateRandom(256);
+     let gval=generateRandom(256);
+     let bval=generateRandom(256);
+     generatedcolor=' rgb(' + rval + ',' + gval + ',' + bval + ')';
+     document.getElementById("value").innerText='The rgba value is' + generatedcolor + " corresponds to";
+     // resetting the result text
+     document.getElementById("result").style.fontSize="30px";
+     document.getElementById("result").style.fontWeight="bold";
+     document.getElementById("result").style.display="flex";
+     document.getElementById("result").innerText="Choose your option!";
+     document.get
+     newgame.play();
+     won=false;
+}
+
+function setcolorsonoptions(){
+      let rval=generateRandom(256);
+      let gval=generateRandom(256);
+      let bval=generateRandom(256);
+     document.getElementById("boxes1").style.backgroundColor="rgb(" + rval +"," + gval + "," + bval + ")";
+
+      rval=generateRandom(256);
+      gval=generateRandom(256);
+      bval=generateRandom(256);
+     document.getElementById("boxes2").style.backgroundColor="rgb(" + rval +"," + gval + "," + bval + ")";
+
+      rval=generateRandom(256);
+      gval=generateRandom(256);
+      bval=generateRandom(256);
+     document.getElementById("boxes3").style.backgroundColor="rgb(" + rval +"," + gval + "," + bval + ")";
+
+      rval=generateRandom(256);
+      gval=generateRandom(256);
+      bval=generateRandom(256);
+
+     document.getElementById("boxes4").style.backgroundColor="rgb(" + rval +"," + gval + "," + bval + ")";
+     var ans="boxes" + correct_choice;
+     var setcolor = ans.toString();
+     document.getElementById(setcolor).style.backgroundColor = generatedcolor;
+}
+
+function iscorrectmatch()
+{
+   window.onclick = e => {
+    
+    // console.log(letter);
+    // const ismatching= letter.toUpperCase() === e.target.innerText.toUpperCase();
+    if(e.target.innerText!=="New")
+    {
+    if(correct_choice === parseInt(e.target.innerText)  && won===false)
+      {console.log("you won!");
+       document.getElementById("result").style.fontSize="30px";
+       document.getElementById("result").style.fontWeight="bold";
+       document.getElementById("result").style.display="flex";
+       document.getElementById("result").innerText="You Won!";
+       win.play();
+       won=true;
+   }
+
+    else 
+      {
+
+        if(won===false)
+        {console.log("you loose");
+      document.getElementById("result").style.fontSize="30px";
+      document.getElementById("result").style.fontWeight="bold";
+      document.getElementById("result").style.display="flex";
+      document.getElementById("result").innerText="You Loose!";
+      lose.play();
+      won=true;
+  }
+  }
+}
+} 
+}
+
+
